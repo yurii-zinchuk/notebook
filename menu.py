8a0af2bf862250d8f4ef4317eef9d7bf17298f1a
Yurii Zinchuk <yurii.zinchuk@ucu.edu.ua>
2022-02-26 22:03:13 +0200

    finished writing Notebook

---
+++ menu.py
@@ -0,0 +1,93 @@
+"""This module contains class to navigate in the Notebook"""
+
+from notebook import *
+import sys
+
+
+class Menu:
+    """
+    A class to navigate in the notebook.
+    """
+
+    def __init__(self):
+        """Initialize a menu, add an empty notebook and choices.
+        """
+        self.notebook = Notebook()
+        self.choices = {
+            "1": self.show_notes,
+            "2": self.search_notes,
+            "3": self.add_note,
+            "4": self.modify_note,
+            "5": self.quit
+        }
+
+    def display_menu(self):
+        """Display menu on the screen.
+        """
+        print("""
+            Notebook Menu
+            1. Show all Notes
+            2. Search Notes
+            3. Add Note
+            4. Modify Note
+            5. Quit
+        """)
+
+    def run(self):
+        """Start executing the program, take user choices.
+        """
+        while True:
+            self.display_menu()
+            choice = input("Enter an option: ")
+            action = self.choices.get(choice)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choice))
+
+    def show_notes(self, notes: Note = None):
+        """Show notes contents.
+
+        Args:
+            notes (Note, optional): Note to show conctents. Defaults to None.
+        """
+        if not notes:
+            notes = self.notebook.notes
+        for note in notes:
+            print("{0}: {1}\n{2}".format(
+                note.id, note.tags, note.memo))
+
+    def search_notes(self):
+        """Searches notes in the notebook and show them.
+        """
+        filterr = input("Search for: ")
+        notes = self.notebook.search(filterr)
+        self.show_notes(notes)
+
+    def add_note(self):
+        """Add a new note to the notebook.
+        """
+        memo = input("Enter a memo: ")
+        self.notebook.new_note(memo)
+        print("Your note has been added.")
+
+    def modify_note(self):
+        """Change memo or tags of the note.
+        """
+        id = input("Enter a note id: ")
+        memo = input("Enter a memo: ")
+        tags = input("Enter tags: ")
+        if memo:
+            self.notebook.modify_memo(id, memo)
+        if tags:
+            self.notebook.modify_tags(id, tags)
+
+    def quit(self):
+        """Stop the program.
+        """
+        print("Thank you for using your notebook today.")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    Menu().run()
---
+++ notebook.py
@@ -0,0 +1,93 @@
+"""This module contains classes for Notebook realization"""
+
+import datetime
+
+
+last_id = 0
+
+
+class Note:
+    """
+    A class to represent single Note.
+    """
+
+    def __init__(self, memo: str, tags=''):
+        """Initialize a note with its meno and tags.
+
+        Args:
+            memo (str): Content of the note.
+            tags (str, optional): Tags of the note. Defaults to ''.
+        """
+        self.memo = memo
+        self.tags = tags
+        self.creation_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id
+
+    def match(self, filter: str) -> bool:
+        """Find whether note matches the search by memo or tags.
+
+        Args:
+            filter (str): Text to match.
+
+        Returns:
+            bool: Whether matches.
+        """
+        return filter in self.memo or filter in self.tags
+
+
+class Notebook:
+    """
+    A class to represent a Notebool consisting of notes.
+    """
+
+    def __init__(self):
+        """Initialize a notebook wiht empty list.
+        """
+        self.notes = []
+
+    def new_note(self, memo: str, tags=''):
+        """Create new note and add ot notebook.
+
+        Args:
+            memo (str): Content of the note.
+            tags (str, optional): Tags of the note. Defaults to ''.
+        """
+        self.notes.append(Note(memo, tags))
+
+    def modify_memo(self, note_id: int, memo: str):
+        """Modify memo of the note with its ID given.
+
+        Args:
+            note_id (int): ID of the note.
+            memo (str): New contents of the note.
+        """
+        for note in self.notes:
+            if note.id == note_id:
+                note.memo = memo
+                break
+
+    def modify_tags(self, note_id: int, tags: str):
+        """Modify meno of the note with its ID given.
+
+        Args:
+            note_id (int): ID of the note.
+            tags (str): New tags of the note.
+        """
+        for note in self.notes:
+            if note.id == note_id:
+                note.tags = tags
+                break
+
+    def search(self, filterr: str) -> list:
+        """Return all notes from the notebook that match the filter.
+
+        Args:
+            filterr (str): Filter to find notes.
+
+        Returns:
+            list: All notes that match.
+        """
+        return [note for note in self.notes if
+                note.match(filterr)]
